f4e7c3ba569cd8d8f786ff5b5bb897acbf075a70
Tiger Wang <tigerwang@outlook.com>
2023-05-22 14:07:39 -0400

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

--- internal/checker.go
+++ internal/checker.go
@@ -1,1 +1,26 @@
 package internal
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/IceWhaleTech/CasaOS-Common/utils/http"
+	"github.com/IceWhaleTech/CasaOS-Installer/codegen"
+)
+
+func GetReleaseFrom(releaseURL string) (*codegen.Release, error) {
+	// download content from releaseURL
+	response, err := http.Get(releaseURL, 30*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	// parse release
+	var release *codegen.Release
+	if err := json.NewDecoder(response.Body).Decode(release); err != nil {
+		return nil, err
+	}
+
+	return release, nil
+}
