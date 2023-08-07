06612dcd230a13709fd38131e0dc4b899fb5a26a
CorrectRoadH <a778917369@gmail.com>
2023-08-07 15:02:19 +0800

    test: enable TestDownloadAndExtract

--- internal/install_test.go
+++ internal/install_test.go
@@ -52,10 +52,6 @@ func TestDownload(t *testing.T) {
 }
 
 func TestDownloadAndExtract(t *testing.T) {
-	if _, exists := os.LookupEnv("CI"); exists {
-		t.Skip("skipping test in CI environment")
-	}
-
 	defer goleak.VerifyNone(t, goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start")) // https://github.com/census-instrumentation/opencensus-go/issues/1191
 
 	logger.LogInitConsoleOnly()
