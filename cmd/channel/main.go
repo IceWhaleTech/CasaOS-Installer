3798ee5dbbe2d1062dc2d22aadf4eb599f2c2c86
CorrectRoad <a778917369@gmail.com>
2024-04-30 18:36:41 +0800

    feat: channel switch tool (#145)

---
+++ cmd/channel/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/IceWhaleTech/CasaOS-Installer/internal/config"
+)
+
+func main() {
+	config.InitSetup(config.InstallerConfigFilePath)
+
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("Please provide a command.")
+		os.Exit(1)
+	}
+
+	switch args[1] {
+	case "public":
+		PublicTestChannel()
+	case "private":
+		PrivateTestChannel()
+	case "stable":
+		StableChannel()
+	default:
+		fmt.Println("unknow channel.")
+		os.Exit(1)
+	}
+}
+
+func Save() {
+	config.Cfg.Section("server").Key("mirrors").SetValue(strings.Join(config.ServerInfo.Mirrors, ","))
+	err := config.Cfg.SaveTo(config.ConfigFilePath)
+	if err != nil {
+		fmt.Printf("Fail to save file: %v", err)
+	}
+}
+
+func PublicTestChannel() {
+	fmt.Println("Public Test Channel")
+	config.ServerInfo.Mirrors = []string{"https://casaos.oss-cn-shanghai.aliyuncs.com/IceWhaleTech/zimaos-rauc/public-test/"}
+	Save()
+}
+
+func PrivateTestChannel() {
+	fmt.Println("Private Test Channel")
+	config.ServerInfo.Mirrors = []string{"https://casaos.oss-cn-shanghai.aliyuncs.com/IceWhaleTech/zimaos-rauc/private-test/"}
+	Save()
+}
+
+func StableChannel() {
+	fmt.Println("Stable Channel")
+	config.ServerInfo.Mirrors = []string{
+		"https://casaos.oss-cn-shanghai.aliyuncs.com/IceWhaleTech/zimaos-rauc/",
+		"https://raw.githubusercontent.com/IceWhaleTech/zimaos-rauc/main/",
+	}
+	Save()
+}
